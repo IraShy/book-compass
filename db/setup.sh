9341a2d632cb07f3cce8fe23644e0b4219230318
IraShy <60783373+IraShy@users.noreply.github.com>
2025-04-19 15:00:06 +1000

    chore: add setup script to initialize PostgreSQL

--- db/setup.sh
+++ db/setup.sh
@@ -4,13 +4,26 @@ DB_USER="bookuser"
 DB_PASS="bookpassword"
 DB_NAME="book_compass_dev"
 
+echo "Dropping database $DB_NAME if it exists..."
+dropdb $DB_NAME --if-exists
+
 echo "Creating database $DB_NAME..."
 createdb $DB_NAME
+
+echo "Dropping user $DB_USER if exists..."
+dropuser $DB_USER --if-exists
 
 echo "Creating user $DB_USER..."
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';" || echo "User $DB_USER already exists."
 
 echo "Granting privileges to $DB_USER on $DB_NAME..."
 psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+
+echo "Setting up schema..."
+# psql -d $DB_NAME -f db/schema.sql
+# psql -U $DB_USER -d $DB_NAME -f db/schema.sql
+PGPASSWORD=$DB_PASS psql -U $DB_USER -d $DB_NAME -f db/schema.sql
+
+
 
 echo "Setup complete."
