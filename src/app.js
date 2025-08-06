b9b4e0b306c1a58e0cd6e8012eff8d8eed936d60
IraShy <60783373+IraShy@users.noreply.github.com>
2025-08-07 01:30:58 +1000

    feat: implement basic CORS config

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
 
@@ -7,6 +8,13 @@ const usersRoutes = require("./routes/users");
 const booksRoutes = require("./routes/books");
 const reviewsRoutes = require("./routes/reviews");
 const { requestLogger } = require("./middlewares/requestLogger");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
